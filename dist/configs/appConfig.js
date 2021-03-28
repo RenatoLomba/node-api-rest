85a856bbf2922a817982b8687e38b1a310d6472a
RenatoLomba <rntlomba@gmail.com>
2021-03-28 13:04:30 -0300

    commit appConfig

--- dist/configs/appConfig.js
+++ dist/configs/appConfig.js
@@ -1,6 +1,6 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true});class Configs {
   constructor() {
-    this.url = process.env.SERVER_URL;
+    this.url = `${process.env.SERVER_URL}:${process.env.SERVER_PORT}`;
   }
 }
 exports. default = new Configs();
